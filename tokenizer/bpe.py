c85354d0f2df174c5d7f9584f57637ee161cfc1b
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2026-03-25 22:38:51 +0530

    create structure for bpe

--- tokenizer/bpe.py
+++ tokenizer/bpe.py
@@ -1,21 +1,21 @@
-import torch
-from collections import Counter
-from typing import List
+from collections import defaultdict
 
 
 class BPE:
     def __init__(self):
         pass
 
+    def build_corpus(self):
+        pass
 
-if __name__ == "__main__":
-    from core.calculate_seq_len import calculate_seq_len
-    data = [
-        "Hello, How are you?",
-        "What are you doing now?",
-        "The smell of fresh rain on dry earth is called petrichor.",
-        "Could you please pass the salt?",
-        "I'm planning to go for a long walk once the sun sets.",
-        "The old lighthouse stood as a silent sentinel against the crashing waves.",
-        "A flicker of doubt crossed his face, but he quickly masked it with a smile"
-    ]
+    def get_pair_count(self):
+        pass
+
+    def merge_pair(self):
+        pass
+
+    def train_bpe(self, corpus):
+        pass
+
+    def tokenize(self, text):
+        pass
